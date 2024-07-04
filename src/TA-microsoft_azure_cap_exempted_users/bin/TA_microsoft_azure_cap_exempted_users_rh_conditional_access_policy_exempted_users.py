d39ce6a7a8670b8f58d58bf1a1957da83be41a1d
morethanyell <daniel.l.astillero@gmail.com>
2024-07-04 19:18:47 +0000

    Version 2

---
+++ src/TA-microsoft_azure_cap_exempted_users/bin/TA_microsoft_azure_cap_exempted_users_rh_conditional_access_policy_exempted_users.py
@@ -0,0 +1,85 @@
+
+import ta_microsoft_azure_cap_exempted_users_declare
+
+from splunktaucclib.rest_handler.endpoint import (
+    field,
+    validator,
+    RestModel,
+    DataInputModel,
+)
+from splunktaucclib.rest_handler import admin_external, util
+from splunk_aoblib.rest_migration import ConfigMigrationHandler
+
+util.remove_http_proxy_env_vars()
+
+
+fields = [
+    field.RestField(
+        'interval',
+        required=True,
+        encrypted=False,
+        default=None,
+        validator=validator.Pattern(
+            regex=r"""^\-[1-9]\d*$|^\d*$""", 
+        )
+    ), 
+    field.RestField(
+        'index',
+        required=True,
+        encrypted=False,
+        default='default',
+        validator=validator.String(
+            min_len=1, 
+            max_len=80, 
+        )
+    ), 
+    field.RestField(
+        'policy_name',
+        required=True,
+        encrypted=False,
+        default='.',
+        validator=validator.String(
+            min_len=0, 
+            max_len=8192, 
+        )
+    ), 
+    field.RestField(
+        'client_id',
+        required=True,
+        encrypted=False,
+        default=None,
+        validator=None
+    ), 
+    field.RestField(
+        'tenant_id',
+        required=True,
+        encrypted=False,
+        default=None,
+        validator=validator.String(
+            min_len=0, 
+            max_len=8192, 
+        )
+    ), 
+
+    field.RestField(
+        'disabled',
+        required=False,
+        validator=None
+    )
+
+]
+model = RestModel(fields, name=None)
+
+
+
+endpoint = DataInputModel(
+    'conditional_access_policy_exempted_users',
+    model,
+)
+
+
+if __name__ == '__main__':
+    admin_external.handle(
+        endpoint,
+        handler=ConfigMigrationHandler,
+    )
